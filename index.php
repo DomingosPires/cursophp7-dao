06187a583719a3ddcd57716758b03a5c43d62edf
Domingos <domingospires288@gmail.com>
2023-01-10 17:48:15 +0000

    criação class usuario

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+/*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo '<pre>';
-echo json_encode($usuarios);
+echo json_encode($usuarios);*/
+
+$root = new Usuario();
+
+$root->loadById(4);
+echo '<pre>';
+echo $root;
+
+
 
 ?>
