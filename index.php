3d41e05922b9fe2627e3d617219111913934344f
Domingos <domingospires288@gmail.com>
2023-01-17 12:16:13 +0000

    metodos de lista

--- index.php
+++ index.php
@@ -9,11 +9,44 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo '<pre>';
 echo json_encode($usuarios);*/
 
-$root = new Usuario();
+/* //////////////////////////// */
+/*      MOSTRA USER BY ID       */
+/* //////////////////////////// */
 
+/*$root = new Usuario();
 $root->loadById(4);
+
 echo '<pre>';
 echo $root;
+
+/* //////////////////////////// */
+/*     MOSTRA TODOS OS USERS    */
+/* //////////////////////////// */
+
+/*$list = Usuario::getList();
+
+echo '<pre>';
+echo json_encode($list);*/
+
+/* //////////////////////////// */
+/*     MOSTRA USERS BY LOGIN    */
+/* //////////////////////////// */
+
+/*$search = Usuario::search("Pi");
+
+echo '<pre>';
+echo json_encode($search);*/
+
+/* //////////////////////////////////////// */
+/*     MOSTRA USERS USANDO LOGIN E SENHA    */
+/* //////////////////////////////////////// */
+
+$usuario = new Usuario();
+
+$usuario->userLogin("Domingos","1234567890");
+
+echo '<pre>';
+echo json_encode($usuario);
 
 
 
